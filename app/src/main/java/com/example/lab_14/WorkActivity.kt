d9025f5c918863af5393a4144d7e61702375a6d3
Yadira-26 <yadirazeballosm@gmail.com>
2025-06-23 23:33:18 -0500

    Implemento segunda vista WorkActivity y actualizo widget con botones para abrir MainActivity y WorkActivity

---
+++ app/src/main/java/com/example/lab_14/WorkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab_14
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lab_14.R
+
+class WorkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work)
+    }
+}
